a5bd31884f8136f45e57161afdeee3e94d6a5ca9
Robert Stefan <rstefan1@users.noreply.github.com>
2019-05-11 19:18:38 +0300

    Add tests for callback registry interface

---
+++ pkg/callback/callback_test.go
@@ -0,0 +1,85 @@
+/*
+Copyright 2019 Robert Andrei STEFAN
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package callback
+
+import (
+	"reflect"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("CallbackRegistry interface", func() {
+	It("creates new registry when given callbacks map is empty", func() {
+		cb := map[string]CallbackFn{}
+		r, err := NewRegistry(cb)
+		Expect(err).To(Succeed())
+		Expect(r.callbacks).To(Equal(cb))
+	})
+
+	It("creates new registry when given callbacks map has more callbacks", func() {
+		cb := map[string]CallbackFn{
+			"first-callback": func(msg string) (bool, error) {
+				return true, nil
+			},
+			"second-callback": func(msg string) (bool, error) {
+				return false, nil
+			},
+		}
+		r, err := NewRegistry(cb)
+		Expect(err).To(Succeed())
+		Expect(r.callbacks).To(Equal(cb))
+	})
+
+	It("returns error if given callbacks map is empty", func() {
+		r, err := NewRegistry(nil)
+		Expect(err).To(Not(Succeed()))
+		Expect(r).To(BeNil())
+	})
+
+	It("returns proper callback func when given callback type exists in registry", func() {
+		var (
+			cbType string
+			cbFn   CallbackFn
+		)
+
+		cbType = "my-callback"
+		cbFn = func(msg string) (bool, error) {
+			return true, nil
+		}
+
+		cb := map[string]CallbackFn{
+			cbType: cbFn,
+		}
+		r, err := NewRegistry(cb)
+		Expect(err).To(Succeed())
+
+		fn, err := r.GetCallback(cbType)
+		Expect(err).To(Succeed())
+		Expect(reflect.ValueOf(fn)).To(Equal(reflect.ValueOf(cbFn)))
+	})
+
+	It("returns error when given callback type doesn't exist in registry", func() {
+		cb := map[string]CallbackFn{}
+		r, err := NewRegistry(cb)
+		Expect(err).To(Succeed())
+
+		fn, err := r.GetCallback("mu-callback")
+		Expect(err).To(Not(Succeed()))
+		Expect(fn).To(BeNil())
+	})
+})
