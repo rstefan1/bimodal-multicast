64def338a1ffcc5b9603d64f33128f7d0facbd2e
Robert Ștefan <rstefan1@users.noreply.github.com>
2022-08-10 20:19:55 +0300

    Bump ginkgo version to v2.1.4

--- pkg/bmmc/gossiper_test.go
+++ pkg/bmmc/gossiper_test.go
@@ -17,7 +17,7 @@ limitations under the License.
 package bmmc
 
 import (
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"github.com/rstefan1/bimodal-multicast/pkg/internal/buffer"
